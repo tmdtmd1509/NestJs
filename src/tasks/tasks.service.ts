70f5d967ccd98faf84d6b23a902b2f7acf72791e
Heeseung Hwang <tmdtmd1509@gmail.com>
2021-04-28 18:49:17 -0600

    finish tasks basic features.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,15 +12,66 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if(status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if(search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)
+            );
+        }
+        
+        return tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
         const task: Task = {
             id: uuid(),
             title,
             description,
             status: TaskStatus.OPEN,
         };
-        
+
         this.tasks.push(task);
         return task;
+    }
+
+    updateTask(id: string, createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
+        const updatedTask: Task = {
+            id,
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        const index = this.tasks.findIndex(task => task.id === id);
+        this.tasks.splice(index, 1, updatedTask);
+        return updatedTask;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+
+    deleteTask(id: string): Task {
+        const index = this.tasks.findIndex(task => task.id === id);
+        return this.tasks.splice(index, 1)[0];
     }
 }
